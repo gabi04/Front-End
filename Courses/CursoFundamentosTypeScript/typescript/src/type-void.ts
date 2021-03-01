76d48d22947aafd5257cc2abaa1584a4853caf76
Gabriela Suárez <gsuarezc@unal.edu.co>
2021-03-01 12:04:05 -0500

    Curso fundamentos de TypeScript

---
+++ Courses/CursoFundamentosTypeScript/typescript/src/type-void.ts
@@ -0,0 +1,53 @@
+// Tipo Void: Representa la ausencia de tipo. usado en funciones que no retornan nada.
+
+// Tipo explicito
+function showInfo(user: any): any {
+  console.log("User Info", user.id, user.username, user.firstName);
+}
+
+showInfo({ id: 1, username: "luixaviles", firstName: "Luis" });
+
+// Tipo Inferido
+function showFormattedInfo(user: any) {
+  console.log(
+    "User Info",
+    `
+        id: ${user.id}
+        username: ${user.username}
+        firstName: ${user.firstName}
+    `
+  );
+}
+
+showFormattedInfo({ id: 1, username: "luixaviles", firstName: "Luis" });
+
+// tipo void, como tipo de dato en variable
+let unusable: void;
+unusable = null;
+unusable = undefined;
+
+// Tipo Never: Representa funciones que lanzan excepciones o nunca retornan un valor.
+
+function handleError(code: number, message: string): never{
+
+  // Process your code here
+  // Generate a message
+  throw new Error(`${message}. Code: ${code}`);
+}
+
+try {
+  handleError(404, 'Not Found');
+} catch (error) {
+}
+
+function sumNumbers(limit: number): never {
+  let sum = 0;
+  while(true) {
+      sum++;
+  }
+  // return sum;
+}
+
+sumNumbers(10);
+// ciclo infinito, programa nunca termina
+
